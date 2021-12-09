91eaaf9d59f78cde168aecbe0c06086b44aa0461
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-12-10 00:18:47 +0100

    initial commit

---
+++ ntrfc/README.rst
@@ -0,0 +1,41 @@
+=====
+NTRfC
+=====
+
+
+.. image:: https://img.shields.io/pypi/v/ntrfc.svg
+        :target: https://pypi.python.org/pypi/ntrfc
+
+.. image:: https://img.shields.io/travis/MaNyh/ntrfc.svg
+        :target: https://travis-ci.com/MaNyh/ntrfc
+
+.. image:: https://readthedocs.org/projects/ntrfc/badge/?version=latest
+        :target: https://ntrfc.readthedocs.io/en/latest/?version=latest
+        :alt: Documentation Status
+
+
+
+Numerical Test Rig for Cascades. A workflow-library for cfd-analysis of cascade-flows
+
+
+* Free software: MIT license
+* Documentation: https://ntrfc.readthedocs.io.
+
+Installation
+--------
+
+install using 'pip install ntrfc'
+or install using 'python -m setup install'
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ ntrfc/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+install_reqs = parse_requirements("requirements.txt",session="hack")
+requirements = [ir.requirement for ir in install_reqs]
+
+#requirements = ["snakemake==6.12.3" ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Malte Nyhuis",
+    author_email='nyhuis@tfd.uni-hannover.de',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Numerical Test Rig for Cascades. A workflow-library for cfd-analysis of cascade-flows",
+    entry_points={
+        'console_scripts': [
+            'ntrfc=ntrfc.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='ntrfc',
+    name='ntrfc',
+    packages=find_packages(include=['ntrfc', 'ntrfc.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/MaNyh/ntrfc',
+    version='0.1.0',
+    zip_safe=False,
+)
