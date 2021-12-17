26d40048867f1c65b0dfb6b1fa2465013cad793f
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-12-17 02:20:25 +0100

    working workflow

--- ntrfc/preprocessing/geometry_creation/profile_pointcloud.py
+++ ntrfc/preprocessing/geometry_creation/profile_pointcloud.py
@@ -1,14 +1,20 @@
-from ntrfc.utils.geometry.pointcloud_methods import ex
+from ntrfc.utils.filehandling.read_datafiles import write_pickle, write_yaml_dict
+from ntrfc.utils.geometry.pointcloud_methods import extract_geo_paras
 
-def generate_profile_pointcloud_geometry(settings):
-    ptcloud_profile = config["blade"]["ptcloud_profile"]
-    ptcloud_profile_unit = config["blade"]["ptcloud_profile_unit"]
-    alpha = config["blade"]["alpha"]
+
+import numpy as np
+import os
+
+def generate_profile_pointcloud_geometry(settings,basedir):
+    ptcloud_profile = settings["ptcloud_profile"]
+    ptcloud_profile_unit = settings["ptcloud_profile_unit"]
+    alpha = settings["alpha"]
 
     # =============================================================================
     # Daten Einlesen
     # =============================================================================
-    points = np.loadtxt(ptcloud_profile)
+    ptcloud_abspath = os.path.join(basedir,ptcloud_profile)
+    points = np.loadtxt(ptcloud_abspath)
     unitcoeff = 0
     if ptcloud_profile_unit == "m":
         unitcoeff = 1
@@ -21,5 +27,18 @@ def generate_profile_pointcloud_geometry(settings):
     # =============================================================================
     sortedPoints, psPoly, ssPoly, ind_vk, ind_hk, midsPoly, beta_meta_01, beta_meta_02, camber_angle = extract_geo_paras(
         points,
-        alpha,
-        verbose)
+        alpha)
+
+    geometry_paras = {"ind_vk":ind_vk,
+                    "ind_hk":ind_hk,
+                    "beta_meta_01":beta_meta_01,
+                    "beta_meta_02":beta_meta_02,
+                    "camber_angle":camber_angle}
+
+
+    geo_dir = os.path.join(basedir,"01_geometry")
+    np.savetxt(os.path.join(geo_dir,"sortedPoints.txt"), sortedPoints)
+    psPoly.save(os.path.join(geo_dir,"psPoly.vtk"),False)
+    ssPoly.save(os.path.join(geo_dir,"ssPoly.vtk"),False)
+    midsPoly.save(os.path.join(geo_dir,"midsPoly.vtk"),False)
+    write_yaml_dict(os.path.join(geo_dir,"geometry_paras.yml"),geometry_paras)
