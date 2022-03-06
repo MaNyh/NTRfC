3da99bb886f2522c008b9ced4aa73ac4417202aa
MaNyh <nyhuis@tfd.uni-hannover.de>
2022-03-06 13:29:31 +0100

    delete unfinished mesh-creation from master

--- examples/gwk_compressor_meshgeneration/development_run.py
+++
@@ -1,5 +0,0 @@
-#import os
-#os.system('snakemake -c1 03_meshgeometry/tfd_gwk_compressor_blocklines.pdf')
-from ntrfc.preprocessing.mesh_creation.domain_creation import create_2d_blocklines
-
-create_2d_blocklines(r"D:\CodingProjects\NTRfC\examples\gwk_compressor",["tfd_gwk_compressor"])
