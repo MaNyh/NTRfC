1e3909bcf2612aa21bd2f44bd39ce673e11b4610
MaNyh <nyhuis@tfd.uni-hannover.de>
2022-05-29 02:52:08 +0200

    Revert "blade-loading implementation"
    This reverts commit 202d590073cf28bda727029a9302e646e3fc5983.

--- ntrfc/utils/filehandling/mesh.py
+++ ntrfc/utils/filehandling/mesh.py
@@ -6,8 +6,9 @@ import pyvista as pv
 def load_mesh(path_to_mesh):
     assert os.path.isfile(path_to_mesh), path_to_mesh + " is not a valid file"
     extension = os.path.splitext(path_to_mesh)[1]
-    if extension == ".vtk" or extension == ".vtp" or extension == ".vtu":
+    if extension == ".vtk":
         mesh = pv.read(path_to_mesh)
+
     elif extension == ".cgns":
         cgns = cgnsReader(path_to_mesh)
         if str(type(cgns)).find('vtkStructuredGrid') != -1:
@@ -15,8 +16,28 @@ def load_mesh(path_to_mesh):
         elif str(type(cgns)).find('vtkUnstructuredGrid') != -1:
             mesh = pv.UnstructuredGrid(cgns)
         elif str(type(cgns)).find('vtkMultiBlockDataSet') != -1:
+            #mesh = pv.UnstructuredGrid()
             multiBlockMesh = pv.MultiBlock(cgns)
+            """for domainId in range(multiBlockMesh.GetNumberOfBlocks()):
+                domain = multiBlockMesh.GetBlock(domainId)
+                for blockId in range(domain.GetNumberOfBlocks()):
+                    block = domain.GetBlock(blockId)
+                    mesh = mesh.merge(block)
+            """
             mesh = multiBlockMesh.combine()
+
+    array_names = mesh.array_names
+    for name in array_names:
+        if name == "Velocity":
+            mesh.rename_array("Velocity", "U")
+        if name == "Pressure":
+            mesh.rename_array("Pressure", "p")
+        if name == "Density":
+            mesh.rename_array("Density", "rho")
+        if name == "Temperature":
+            mesh.rename_array("Temperature", "T")
+    mesh = mesh.cell_data_to_point_data()
+
     return mesh
 
 
