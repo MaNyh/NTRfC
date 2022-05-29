4b8b0affa7cc5e83b8be7f35fecf1fde14a06a81
MaNyh <nyhuis@tfd.uni-hannover.de>
2022-05-29 02:52:07 +0200

    Revert "default names for massflow_plane variables"
    This reverts commit 4de463d6c8fe74abb9782ed2a027b19c2032afbf.

--- ntrfc/utils/pyvista_utils/plane.py
+++ ntrfc/utils/pyvista_utils/plane.py
@@ -3,35 +3,34 @@ import numpy as np
 from ntrfc.utils.math.vectorcalc import vecAbs, vecProjection
 
 
-def massflow_plane(mesh, rhoname="rho", velocityname="U"):
+def massflow_plane(mesh):
     if not "Normals" in mesh.array_names:
         mesh = mesh.compute_normals()
     if not "Area" in mesh.array_names:
         mesh = mesh.compute_cell_sizes()
     mesh = mesh.point_data_to_cell_data()
     normals = mesh.cell_normals
-    rhos = mesh[rhoname]
+    rhos = mesh["rho"]
     areas = mesh["Area"]
-    velocities = mesh[velocityname]
+    velocities = mesh["U"]
 
     massflow = np.array(
         [vecAbs(vecProjection(velocities[i], normals[i])) for i in range(mesh.number_of_cells)]) ** 2 * rhos * areas
 
     return massflow
 
+def massflowave_plane(mesh,valname):
+    massflow = massflow_plane(mesh)
 
-def massflowave_plane(mesh, valname, rhoname="rho", velocityname="U"):
-    massflow = massflow_plane(mesh, rhoname=rhoname, velocityname=velocityname)
-
-    mass_ave = np.sum(mesh[valname] * massflow) / np.sum(massflow)
+    mass_ave = np.sum(mesh[valname]*massflow)/np.sum(massflow)
 
     return mass_ave
 
 
 def areaave_plane(mesh, valname):
-    array = mesh[valname]
+    array=mesh[valname]
     if not "Area" in mesh.array_names:
         mesh = mesh.compute_cell_sizes()
     areas = mesh["Area"]
-    area_ave = np.sum((array.T * areas).T) / np.sum(areas)
+    area_ave = np.sum((array.T*areas).T)/np.sum(areas)
     return area_ave
