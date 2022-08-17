8dcc46a6a3947201c555cfe92f5e90c431535469
Rishab <rishabrk11@gmail.com>
2022-08-17 20:55:03 +0200

    add signal generator to test function

--- tests/test_ntrfc_uncertainties.py
+++ tests/test_ntrfc_uncertainties.py
@@ -1,11 +1,19 @@
 
-
 def test_stationarity_uncertainties():
     from ntrfc.postprocessing.timeseries.uncertainties import stationarity_uncertainties
     import numpy as np
-
-    timesteps = np.arange(0,60,0.001)
-    values = np.sin(timesteps)
+    def gen_sine():
+        amplitude = 1
+        time = 60
+        frequency = 2
+        timesteps = np.arange(0, time, 0.001)
+        values = amplitude * np.sin(frequency * timesteps)
+        return timesteps, values
+    timesteps,values= gen_sine()
 
     ans = stationarity_uncertainties(timesteps,values)
+
     return 0
+
+
+
