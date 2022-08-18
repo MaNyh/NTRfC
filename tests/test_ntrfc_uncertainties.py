07b4596b896651823b25012bbe2a697c83c2c2a3
many <VC6l9uBUTvTlcIjrI7sn>
2022-08-19 01:34:52 +0200

    initial test-function for a transient signal (abating sine)

--- tests/test_ntrfc_uncertainties.py
+++ tests/test_ntrfc_uncertainties.py
@@ -1,30 +1,63 @@
 
-def test_stationarity_uncertainties():
+def test_stationarity_uncertainties_stationarysine():
     from ntrfc.postprocessing.timeseries.uncertainties import stationarity_uncertainties
     import numpy as np
+    from itertools import product
 
     def signalgen_sine(amplitude,frequency,mean,time):
-        resolution = 200
-        step = (resolution*frequency**-1)**-1
+        frequency_resolution = 200
+        step = (frequency_resolution*frequency**-1)**-1
         times = np.arange(0, time, step)
         values = amplitude * np.sin(frequency*(2*np.pi) * times) + mean
         return times, values
 
-    # def signalgen_constant(mean,time):
-    #     resolution = 200
-    #     step = 1/(resolution*time)
-    #     times = np.arange(0, time, step)
-    #     values = np.ones(len(times))*mean
-    #     return times, values
+    test_amplitudes = [1,10]
+    test_frequencies = [1,10,100,1000,10000]
+    test_times = [60]
+    test_mean = [0,1,-1]
 
-    timesteps, values = signalgen_sine(amplitude=1, frequency=1, mean =1, time=2)
+    test_configs = list(product(test_amplitudes,test_frequencies, test_times,test_mean))
 
-    stationary_timestep , uncertainty = stationarity_uncertainties(timesteps,values)
+    for amplitude, frequency, time, mean in test_configs:
+        timesteps, values = signalgen_sine(amplitude=amplitude, frequency=frequency, mean =mean, time=time)
+        stationary_timestep , uncertainty = stationarity_uncertainties(timesteps,values)
+        #todo: implement analytical or value-defined limits for stationarity-approximations.
+        well_computed_stationarity_limit = 0.0
+        well_computed_uncertainty_limit = amplitude
+        assert stationary_timestep == well_computed_stationarity_limit, "computation failed"
+        #todo: define uncertainty limit properly
+        assert well_computed_uncertainty_limit > uncertainty, "computation failed"
 
-    assert stationary_timestep == 0.0 , "computation failed"
-    #todo: define uncertainty limit properly
-    assert 1 > uncertainty , "computation failed"
+def test_stationarity_uncertainties_abatingsine():
+    from ntrfc.postprocessing.timeseries.uncertainties import stationarity_uncertainties
+    import numpy as np
+    from itertools import product
 
+    def signalgen_abatingsine(amplitude,frequency,mean,abate,time):
+        resolution = 200
+        step = (resolution*frequency**-1)**-1
+        times = np.arange(0, time, step)
+        values = amplitude * np.sin(frequency*(2*np.pi) * times) + mean + np.e**-(times*abate)
+        return times, values
+
+    test_amplitudes = [1,10]
+    test_frequencies = [1,10,100,1000,10000]
+    test_times = [60]
+    test_mean = [0,1,-1]
+    test_abate = [1,10]
+
+    test_configs = list(product(test_amplitudes,test_frequencies, test_times,test_mean,test_abate))
+
+    for amplitude, frequency, time, mean, abate in test_configs:
+        timesteps, values = signalgen_abatingsine(amplitude=amplitude, frequency=frequency, mean =mean, time=time, abate=abate)
+        stationary_timestep , uncertainty = stationarity_uncertainties(timesteps,values)
+
+        #todo: implement analytical or value-defined limits for stationarity-approximations.
+        well_computed_stationarity_limit = 0.0
+        well_computed_uncertainty_limit = amplitude
+        assert stationary_timestep == well_computed_stationarity_limit, "computation failed"
+        #todo: define uncertainty limit properly
+        assert well_computed_uncertainty_limit > uncertainty, "computation failed"
 
 
 
