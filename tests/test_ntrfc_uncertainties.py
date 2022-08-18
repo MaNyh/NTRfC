c48b9fc8ff3f36e2e1682ea6cf1b63821f414d40
many <VC6l9uBUTvTlcIjrI7sn>
2022-08-18 02:17:14 +0200

    add mean value to sine

--- tests/test_ntrfc_uncertainties.py
+++ tests/test_ntrfc_uncertainties.py
@@ -3,14 +3,21 @@ def test_stationarity_uncertainties():
     from ntrfc.postprocessing.timeseries.uncertainties import stationarity_uncertainties
     import numpy as np
 
-    def signalgen_sine(amplitude,frequency,time):
+    def signalgen_sine(amplitude,frequency,mean,time):
         resolution = 200
         step = (resolution*frequency**-1)**-1
         times = np.arange(0, time, step)
-        values = amplitude * np.sin(frequency*(2*np.pi) * times)
+        values = amplitude * np.sin(frequency*(2*np.pi) * times) + mean
         return times, values
 
-    timesteps, values = signalgen_sine(amplitude=1, frequency=1, time=2)
+    # def signalgen_constant(mean,time):
+    #     resolution = 200
+    #     step = 1/(resolution*time)
+    #     times = np.arange(0, time, step)
+    #     values = np.ones(len(times))*mean
+    #     return times, values
+
+    timesteps, values = signalgen_sine(amplitude=1, frequency=1, mean =1, time=2)
 
     ans = stationarity_uncertainties(timesteps,values)
 
