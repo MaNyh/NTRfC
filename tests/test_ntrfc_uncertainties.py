9a87741b9ab7c795e7e2e2dbb5f33a5291101feb
many <VC6l9uBUTvTlcIjrI7sn>
2022-08-18 02:05:32 +0200

    polish signal generator

--- tests/test_ntrfc_uncertainties.py
+++ tests/test_ntrfc_uncertainties.py
@@ -2,14 +2,15 @@
 def test_stationarity_uncertainties():
     from ntrfc.postprocessing.timeseries.uncertainties import stationarity_uncertainties
     import numpy as np
-    def gen_sine():
-        amplitude = 1
-        time = 60
-        frequency = 2
-        timesteps = np.arange(0, time, 0.001)
-        values = amplitude * np.sin(frequency * timesteps)
-        return timesteps, values
-    timesteps,values= gen_sine()
+
+    def signalgen_sine(amplitude,frequency,time):
+        resolution = 200
+        step = (resolution*frequency**-1)**-1
+        times = np.arange(0, time, step)
+        values = amplitude * np.sin(frequency*(2*np.pi) * times)
+        return times, values
+
+    timesteps, values = signalgen_sine(amplitude=1, frequency=1, time=2)
 
     ans = stationarity_uncertainties(timesteps,values)
 
