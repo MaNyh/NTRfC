12d03d201a20600fa23474c5096f61807ab934ff
many <VC6l9uBUTvTlcIjrI7sn>
2022-08-19 23:34:15 +0200

    new approach from old ntrfc jupyter-notebook stat.ipynb

--- ntrfc/postprocessing/timeseries/uncertainties.py
+++ ntrfc/postprocessing/timeseries/uncertainties.py
@@ -3,6 +3,16 @@ import matplotlib.pyplot as plt
 import bisect
 from scipy.integrate import simps
 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import scipy.stats
+
+
+
+
+
+
 
 def stationarity_uncertainties(timesteps, values, verbose=False):
     '''
@@ -21,83 +31,113 @@ def stationarity_uncertainties(timesteps, values, verbose=False):
 
     # we are integrating from zero to zero-crossing in the autocorrelation, we need the time to begin with zeros
     # probably the used datasample is not beginning with 0. therefore:
-    timesteps -= timesteps[0]  ###### revDA: timeSteps start at zero before signal is divided into two parts
-    #################################################
 
-    # this is for the full signal (technically not needed)
-    # signal_mean = np.mean(values)
+    time = timesteps
 
-    # this is for half the signal
-    second_half_id = int(len(values) / 2)
-    second_half_of_signal = np.copy(values[second_half_id:])
-    second_half_mean = np.mean(second_half_of_signal)
-    second_half_of_signal_norm = second_half_of_signal - second_half_mean  ## the autocorrelation function needs a signal that fluctuates around 0
-    autocorrelated = autocorr(second_half_of_signal_norm)
-    second_half_timesteps = np.copy(timesteps[second_half_id:])
+    somesignal = values
 
-    if len(zero_crossings(autocorrelated)) > 0:
-        acorr_zero_crossings = zero_crossings(autocorrelated)[0]
-    else:
-        print("no zero crossing found, using first minimal value (possibly last timestep). check data quality!")
-        acorr_zero_crossings = np.where(autocorrelated == min(autocorrelated))[0][0]
+    error = 0.00005
 
-    integral_scale = simps(autocorrelated[:acorr_zero_crossings], timesteps[:acorr_zero_crossings])
+    def mean_confidence_interval(data, confidence=0.95):
+        a = 1.0 * np.array(data)
+        n = len(a)
+        m, se = np.mean(a), scipy.stats.sem(a)
+        h = se * scipy.stats.t.ppf((1 + confidence) / 2., n - 1)
+        return m, m - h, m + h
 
-    # calculate the moving average for every 30*integral_scale time intervals
-    integrals_window = 30
-    t_window_size = integrals_window * integral_scale
-    t_window_len = bisect.bisect(timesteps, t_window_size)
-    # calculate moving average with t_window_len
-    signal_SMA = moving_average(values, t_window_len)
+    def statistical_timewindow(somesignal, time, error):
+        numtimesteps = len(time)
+        dt = time[1] - time[0]
 
-    eps_time_mean = (np.std(second_half_of_signal) / second_half_mean) * (
-        2 * integral_scale / (second_half_timesteps[-1] - second_half_timesteps[0])) ** .5
-    # eps_time_rms = (integral_scale / (second_half_timesteps[-1] - second_half_timesteps[0])) ** .5
+        minobservations_in_window = int(len(somesignal) / 100) if int(len(somesignal) / 100) > 256 else 256  # 256
+        jumpsteps = 1
+        windows_idxs = [numtimesteps - i for i in np.arange(minobservations_in_window, numtimesteps, jumpsteps)]
 
-    n_windows_mean = len(signal_SMA)
-    # n_windows_rms = len(windows_rms)
+        pspecs = []
+        times = []
+        for check_id in windows_idxs:
+            signal_window = somesignal[check_id:]
+            time_window = time[check_id:]
 
-    confidence_level = 1.96
-    confidence_mean_high = second_half_mean * (1 + confidence_level * eps_time_mean)
-    confidence_mean_low = second_half_mean * (1 - confidence_level * eps_time_mean)
-    # confidence_rms_high = np.mean(windows_rms) * (1 + 1.96 * eps_time_rms)
-    # confidence_rms_low = np.mean(windows_rms) * (1 - 1.96 * eps_time_rms)
+            fs = dt ** -1
+            f, Pxx_spec = signal.welch(signal_window, fs, 'flattop', scaling='spectrum')
+            pspecs.append(Pxx_spec)
+            times.append(time_window[0])
+            if len(pspecs) > 2:
+                pspec_diff_sqr = np.abs(pspecs[-2] - pspecs[-1]) / pspecs[
+                    -1]  # **2/(pspecs[-2]*pspecs[-1])#/(times[-1]-times[2])
+                pspecs_diff_sqrint = np.trapz(pspec_diff_sqr, f) / np.trapz(pspecs[-1], f)  # ,fr)/(times[-2]-times[-1])
+                if pspecs_diff_sqrint < error:
+                    # statistical_convergence_timestep = check_id
 
-    mean_in_ci_range = [True if (confidence_mean_high >= i >= confidence_mean_low) else False for i in signal_SMA]
-    # rms_in_ci_range = [True if (confidence_rms_high >= i >= confidence_rms_low) else False for i in windows_rms]
+                    return check_id, pspecs, times
 
-    mean_stationarity_fraction = np.array(
-        [sum(mean_in_ci_range[pt:]) / (n_windows_mean - pt) for pt in range(len(mean_in_ci_range))])
-    # rms_stationarity_fraction = np.array(
-    #     [sum(rms_in_ci_range[pt:]) / (n_windows_rms - pt) for pt in range(len(rms_in_ci_range))])
+        return -1, -1, -1
 
-    confidence_threshold = 0.95
-    id_stationary = bisect.bisect(mean_stationarity_fraction, confidence_threshold)
+    stationarity_window_timestep, pspecs, times = statistical_timewindow(somesignal, time, error)
+    print(stationarity_window_timestep)
+    n = len(time)
+    sample_length = n - stationarity_window_timestep
 
-    if verbose==True:
-        fig, axs = plt.subplots(2, 1)
-        axs[0].plot(values)
-        axs[0].hlines(confidence_mean_high, xmin=0, xmax=len(values), colors="cyan")
-        axs[0].hlines(confidence_mean_low, xmin=0, xmax=len(values), colors="cyan")
-        axs[0].plot(signal_SMA, "k", linewidth=.5)
-        axs[0].vlines(id_stationary, ymin=min(values), ymax=max(values), colors="red")
-        axs[1].plot(autocorrelated)
-        plt.show()
+    plt.figure()
+    plt.hist(somesignal[stationarity_window_timestep:], bins=100)
+    plt.show()
 
-    return timesteps[id_stationary], (confidence_mean_high-second_half_mean)
+    plt.figure()
+    plt.plot(somesignal[stationarity_window_timestep:])
+    plt.show()
+
+    samplesignal = somesignal[stationarity_window_timestep:]
+    sampletime = time[stationarity_window_timestep:]
+    dt = time[1] - time[0]
+
+    stationary = [stationarity_window_timestep, n]
+    checkstationary = [0, stationarity_window_timestep - 1]
+    nonstationary = [None, None]
+
+    searchbool = True
+    new_sample_idx = stationary[0]
+    new_sample_idxbegin = stationary[0]-sample_length
+
+    newsamplesignal = somesignal[new_sample_idxbegin:new_sample_idx]
+    newsampletime = time[new_sample_idxbegin:new_sample_idx]
+    # while(searchbool):
+    fs = dt ** -1
+    f, Pxx_spec = signal.welch(newsamplesignal, fs, 'flattop', scaling='spectrum')
+
+    pspec_diff_sqr = np.abs(Pxx_spec - pspecs[-1]) / pspecs[-1]  # **2/(pspecs[-2]*pspecs[-1])#/(times[-1]-times[2])
+    pspecs_diff_sqrint = np.trapz(pspec_diff_sqr, f) / np.trapz(pspecs[-1], f)  # ,fr)/(times[-2]-times[-1])
+    nerr = (error + error) / (1 - 2 * error) ** 2
+    nnerr = 2 * nerr
+    print(pspecs_diff_sqrint < nnerr)
+    print(pspecs_diff_sqrint)
+
+    plt.figure()
+    plt.plot(f, Pxx_spec)
+    plt.plot(f, pspecs[-1])
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.plot()
+
+    plt.figure()
+    plt.plot(pspec_diff_sqr)
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.plot()
+
+    plt.figure()
+    plt.plot(time, somesignal)
+    plt.axvline(sampletime[-1], color="red")
+    plt.axvline(sampletime[0], color="red")
+    plt.plot(newsampletime, newsamplesignal, color="orange")
+    plt.axvline(time[new_sample_idxbegin], color="black", linestyle="dashed")
+    plt.axvline(time[new_sample_idx], color="black", linestyle="dashed")
+    plt.ylim(min(somesignal), max(somesignal))
+    plt.show()
+
+    id_stationary=new_sample_idxbegin
+
+    return timesteps[id_stationary], time[new_sample_idxbegin]
 
 
 
-def zero_crossings(data_series):
-    zcs = np.where(np.diff(np.sign(data_series)))[0]
-    return zcs
-
-
-def autocorr(x):
-    norm = np.sum(np.array(x) ** 2)
-    result = np.correlate(np.array(x), np.array(x), 'full') / norm
-    return result[int(len(result) / 2):]
-
-
-def moving_average(x, w):
-    return np.convolve(x, np.ones(w), 'valid') / w
