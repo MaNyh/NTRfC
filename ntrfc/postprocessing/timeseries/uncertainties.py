28cab2b90ce658470b5399c0a9ee625837b70613
many <VC6l9uBUTvTlcIjrI7sn>
2022-08-20 02:23:21 +0200

    define stationarity-check, new approach

--- ntrfc/postprocessing/timeseries/uncertainties.py
+++ ntrfc/postprocessing/timeseries/uncertainties.py
@@ -1,20 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import bisect
-from scipy.integrate import simps
-
-import numpy as np
-import matplotlib.pyplot as plt
 from scipy import signal
-import scipy.stats
 
 
+def minimal_statistical_timewindow(somesignal, time, error):
+    """
+    computes the minimal statistical time window for a given error in the energy-spectrum of a signal
+    if the energy-stectrum is converged, a necessary condition for stationarity is satisfied
 
+    """
+    numtimesteps = len(time)
+    dt = time[1] - time[0]
 
+    minobservations_in_window = int(len(somesignal) / 100) if int(len(somesignal) / 100) > 256 else 256  # 256
+    jumpsteps = 1
+    windows_idxs = [numtimesteps - i for i in np.arange(minobservations_in_window, numtimesteps, jumpsteps)]
 
+    pspecs = []
+    times = []
+    for check_id in windows_idxs:
+        # choosing window of the signal. starting from end, iterate to a longer signal towards beginning
+        signal_window = somesignal[check_id:]
+        time_window = time[check_id:]
 
+        #compute powerspectrum
+        fs = dt ** -1
+        f, Pxx_spec = signal.welch(signal_window, fs, 'flattop', scaling='spectrum')
+        pspecs.append(Pxx_spec)
+        times.append(time_window[0])
 
-def stationarity_uncertainties(timesteps, values, verbose=False):
+        if len(pspecs) > 2:
+            pspec_diff_sqr = np.abs(pspecs[-2] - pspecs[-1]) / pspecs[-1]
+            pspecs_diff_sqrint = np.trapz(pspec_diff_sqr, f) / np.trapz(pspecs[-1], f)
+
+            if pspecs_diff_sqrint < error:
+                return check_id, pspecs, times
+
+    return -1, -1, -1
+
+def stationarity_uncertainties(timesteps, values, verbose=True):
     '''
     :param timesteps: 1D np.array()
     :param values: 1D np.array()
@@ -22,122 +46,76 @@ def stationarity_uncertainties(timesteps, values, verbose=False):
     :return: stationary_timestep ,mean_value , uncertainty
     '''
 
-    ### commented is for rms
-
-    # confidence_mean_high = [];  # upper limit
-    # confidence_mean_low = [];  # lower limit
-    # mean_in_ci_range = [];  # is value in confidence range?
-    # out_of_range = [];
-
-    # we are integrating from zero to zero-crossing in the autocorrelation, we need the time to begin with zeros
-    # probably the used datasample is not beginning with 0. therefore:
 
     time = timesteps
-
     somesignal = values
 
-    error = 0.00005
-
-    def mean_confidence_interval(data, confidence=0.95):
-        a = 1.0 * np.array(data)
-        n = len(a)
-        m, se = np.mean(a), scipy.stats.sem(a)
-        h = se * scipy.stats.t.ppf((1 + confidence) / 2., n - 1)
-        return m, m - h, m + h
-
-    def statistical_timewindow(somesignal, time, error):
-        numtimesteps = len(time)
-        dt = time[1] - time[0]
-
-        minobservations_in_window = int(len(somesignal) / 100) if int(len(somesignal) / 100) > 256 else 256  # 256
-        jumpsteps = 1
-        windows_idxs = [numtimesteps - i for i in np.arange(minobservations_in_window, numtimesteps, jumpsteps)]
-
-        pspecs = []
-        times = []
-        for check_id in windows_idxs:
-            signal_window = somesignal[check_id:]
-            time_window = time[check_id:]
-
-            fs = dt ** -1
-            f, Pxx_spec = signal.welch(signal_window, fs, 'flattop', scaling='spectrum')
-            pspecs.append(Pxx_spec)
-            times.append(time_window[0])
-            if len(pspecs) > 2:
-                pspec_diff_sqr = np.abs(pspecs[-2] - pspecs[-1]) / pspecs[
-                    -1]  # **2/(pspecs[-2]*pspecs[-1])#/(times[-1]-times[2])
-                pspecs_diff_sqrint = np.trapz(pspec_diff_sqr, f) / np.trapz(pspecs[-1], f)  # ,fr)/(times[-2]-times[-1])
-                if pspecs_diff_sqrint < error:
-                    # statistical_convergence_timestep = check_id
-
-                    return check_id, pspecs, times
-
-        return -1, -1, -1
-
-    stationarity_window_timestep, pspecs, times = statistical_timewindow(somesignal, time, error)
-    print(stationarity_window_timestep)
-    n = len(time)
-    sample_length = n - stationarity_window_timestep
-
-    plt.figure()
-    plt.hist(somesignal[stationarity_window_timestep:], bins=100)
-    plt.show()
-
-    plt.figure()
-    plt.plot(somesignal[stationarity_window_timestep:])
-    plt.show()
-
-    samplesignal = somesignal[stationarity_window_timestep:]
-    sampletime = time[stationarity_window_timestep:]
+    error = 0.01
+    error_sec = 0.05
     dt = time[1] - time[0]
+    n = len(time)
 
-    stationary = [stationarity_window_timestep, n]
-    checkstationary = [0, stationarity_window_timestep - 1]
-    nonstationary = [None, None]
+    # compute minimal time window for the approximation of a stationary signal
+    minimal_stationarity_timestep, pspecs, times = minimal_statistical_timewindow(somesignal, time, error)
 
-    searchbool = True
-    new_sample_idx = stationary[0]
-    new_sample_idxbegin = stationary[0]-sample_length
-
-    newsamplesignal = somesignal[new_sample_idxbegin:new_sample_idx]
-    newsampletime = time[new_sample_idxbegin:new_sample_idx]
-    # while(searchbool):
-    fs = dt ** -1
-    f, Pxx_spec = signal.welch(newsamplesignal, fs, 'flattop', scaling='spectrum')
-
-    pspec_diff_sqr = np.abs(Pxx_spec - pspecs[-1]) / pspecs[-1]  # **2/(pspecs[-2]*pspecs[-1])#/(times[-1]-times[2])
-    pspecs_diff_sqrint = np.trapz(pspec_diff_sqr, f) / np.trapz(pspecs[-1], f)  # ,fr)/(times[-2]-times[-1])
-    nerr = (error + error) / (1 - 2 * error) ** 2
-    nnerr = 2 * nerr
-    print(pspecs_diff_sqrint < nnerr)
-    print(pspecs_diff_sqrint)
-
-    plt.figure()
-    plt.plot(f, Pxx_spec)
-    plt.plot(f, pspecs[-1])
-    plt.yscale("log")
-    plt.xscale("log")
-    plt.plot()
-
-    plt.figure()
-    plt.plot(pspec_diff_sqr)
-    plt.yscale("log")
-    plt.xscale("log")
-    plt.plot()
-
-    plt.figure()
-    plt.plot(time, somesignal)
-    plt.axvline(sampletime[-1], color="red")
-    plt.axvline(sampletime[0], color="red")
-    plt.plot(newsampletime, newsamplesignal, color="orange")
-    plt.axvline(time[new_sample_idxbegin], color="black", linestyle="dashed")
-    plt.axvline(time[new_sample_idx], color="black", linestyle="dashed")
-    plt.ylim(min(somesignal), max(somesignal))
-    plt.show()
-
-    id_stationary=new_sample_idxbegin
-
-    return timesteps[id_stationary], time[new_sample_idxbegin]
+    #
+    sample_length = n - minimal_stationarity_timestep
 
 
+    error_sec_test = 0
+    checks = 0
+    sample_start = minimal_stationarity_timestep
+    new_sample_idxbegin = n
+    while(error_sec >error_sec_test and sample_start > 0):
+        stationary = [sample_start, n-checks]
 
+        sampletime = time[stationary[0]:stationary[-1]]
+
+        newsamplesignal = somesignal[stationary[0]:stationary[-1]]
+
+        fs = dt ** -1
+        f, Pxx_spec = signal.welch(newsamplesignal, fs, 'flattop', scaling='spectrum')
+
+        pspec_diff_sqr = np.abs(Pxx_spec - pspecs[-1]) / pspecs[-1]
+        pspecs_diff_sqrint = np.trapz(pspec_diff_sqr, f) / np.trapz(pspecs[-1], f)
+        error_sec_test = pspecs_diff_sqrint#(error + error) / (1 - 2 * error) ** 2
+
+        checks+=1
+        sample_start-=1
+
+    id_stationary=new_sample_idxbegin-new_sample_idxbegin
+
+    if verbose:
+        plt.figure()
+        plt.hist(somesignal[minimal_stationarity_timestep:], bins=100)
+        plt.show()
+
+        plt.figure()
+        plt.plot(somesignal[minimal_stationarity_timestep:])
+        plt.show()
+
+        plt.figure()
+        plt.plot(f, Pxx_spec)
+        plt.plot(f, pspecs[-1])
+        plt.yscale("log")
+        plt.xscale("log")
+        plt.plot()
+
+        plt.figure()
+        plt.plot(pspec_diff_sqr)
+        plt.yscale("log")
+        plt.xscale("log")
+        plt.plot()
+
+        plt.figure()
+        plt.plot(time, somesignal)
+        plt.axvline(sampletime[-1], color="red")
+        plt.axvline(sampletime[0], color="red")
+        plt.plot(time[id_stationary:], somesignal[id_stationary:], color="orange")
+        plt.axvline(time[id_stationary], color="black", linestyle="dashed")
+        plt.ylim(min(somesignal), max(somesignal))
+        plt.show()
+
+
+
+    return timesteps[id_stationary], np.std(somesignal[id_stationary:])
--- tests/test_ntrfc_uncertainties.py
+++ tests/test_ntrfc_uncertainties.py
@@ -5,19 +5,29 @@ def test_stationarity_uncertainties_stationarysine():
 
     def signalgen_sine(amplitude, frequency, mean, time):
         frequency_resolution = 200
-        step = (frequency_resolution * frequency ** -1) ** -1
+        tau = frequency ** -1
+
+        step = tau / frequency_resolution
         times = np.arange(0, time, step)
         values = amplitude * np.sin(frequency * (2 * np.pi) * times) + mean
         return times, values
 
     test_amplitudes = [1, 10]
-    test_frequencies = [1, 10, 100, 1000, 10000]
-    test_times = [60]
+    test_frequencies = [1, 10]
+    test_times = [10, 60]
     test_mean = [0, 1, -1]
+
+    maxperiods = -1
+    minperiods = np.inf
 
     test_configs = list(product(test_amplitudes, test_frequencies, test_times, test_mean))
 
     for amplitude, frequency, time, mean in test_configs:
+        equals_periods = time / frequency ** -1
+        if equals_periods > maxperiods:
+            maxperiods = int(equals_periods)
+        if minperiods > equals_periods:
+            minperiods = int(equals_periods)
         timesteps, values = signalgen_sine(amplitude=amplitude, frequency=frequency, mean=mean, time=time)
         stationary_timestep, uncertainty = stationarity_uncertainties(timesteps, values)
         # todo: implement analytical or value-defined limits for stationarity-approximations.
@@ -26,6 +36,7 @@ def test_stationarity_uncertainties_stationarysine():
         assert stationary_timestep == well_computed_stationarity_limit, "computation failed"
         # todo: define uncertainty limit properly
         assert well_computed_uncertainty_limit > uncertainty, "computation failed"
+    assert 10 > minperiods, f"critical value {minperiods} not tested"
 
 
 def test_stationarity_uncertainties_abatingsine():
@@ -41,14 +52,23 @@ def test_stationarity_uncertainties_abatingsine():
         return times, values
 
     test_amplitudes = [1, 10]
-    test_frequencies = [1, 10, 100, 1000, 10000]
-    test_times = [60]
+    test_frequencies = [1, 10]
+    test_times = [10, 60]
     test_mean = [0, 1, -1]
     test_abate = [1, 10]
+
+    maxperiods = -1
+    minperiods = np.inf
 
     test_configs = list(product(test_amplitudes, test_frequencies, test_times, test_mean, test_abate))
 
     for amplitude, frequency, time, mean, abate in test_configs:
+        equals_periods = time / frequency ** -1
+        if equals_periods > maxperiods:
+            maxperiods = int(equals_periods)
+        if minperiods > equals_periods:
+            minperiods = int(equals_periods)
+
         timesteps, values = signalgen_abatingsine(amplitude=amplitude, frequency=frequency, mean=mean, time=time,
                                                   abate=abate)
         stationary_timestep, uncertainty = stationarity_uncertainties(timesteps, values)
@@ -59,3 +79,4 @@ def test_stationarity_uncertainties_abatingsine():
         assert stationary_timestep == well_computed_stationarity_limit, "computation failed"
         # todo: define uncertainty limit properly
         assert well_computed_uncertainty_limit > uncertainty, "computation failed"
+    assert 10 > minperiods, f"critical value {minperiods} not tested"
